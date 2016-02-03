23905339fbb42d374a45552a0332e6c7ddeaa035
Heewa Barfchin <heewa.b@gmail.com>
2016-02-03 15:57:43 -0500

    Split out info about service into a struct

--- service/service.go
+++ service/service.go
@@ -4,19 +4,29 @@ import (
 	"fmt"
 	"os/exec"
 	"os/user"
+	"strings"
 )
 
-type Service struct {
-	// Read-only, ignored at runtime
+// Info holds info about a service
+type Info struct {
 	Name    string
 	Program string
 	Args    []string
 	Dir     string
 	Env     map[string]string
 
-	cmd *exec.Cmd
+	Running   bool
+	Pid       int
+	Succeeded bool
 }
 
+// Service -
+type Service struct {
+	Name string
+	cmd  *exec.Cmd
+}
+
+// New creates a new Service
 func New(name string, program string, args []string, dir string, env map[string]string) (*Service, error) {
 	if name == "" {
 		name = program
@@ -51,21 +61,52 @@ func New(name string, program string, args []string, dir string, env map[string]
 	}
 
 	return &Service{
-		Name:    program,
-		Program: programPath,
-
-		Args: args,
-		Dir:  cmd.Dir,
-		Env:  env,
-
-		cmd: cmd,
+		Name: program,
+		cmd:  cmd,
 	}, nil
 }
 
+// Info gets info about the service
+func (s *Service) Info() Info {
+	info := Info{
+		Name: s.Name,
+		Dir:  s.cmd.Dir,
+	}
+
+	if len(s.cmd.Args) >= 1 {
+		info.Program = s.cmd.Args[0]
+	}
+	if len(s.cmd.Args) >= 2 {
+		info.Args = s.cmd.Args[1:]
+	}
+
+	info.Env = make(map[string]string)
+	for _, item := range s.cmd.Env {
+		parts := strings.Split(item, "=")
+		if len(parts) == 2 {
+			info.Env[parts[0]] = parts[1]
+		} else {
+			info.Env[parts[0]] = ""
+		}
+	}
+
+	if s.Running() {
+		info.Running = true
+		info.Pid = s.cmd.Process.Pid
+	} else {
+		info.Pid = s.cmd.ProcessState.Pid()
+		info.Succeeded = s.cmd.ProcessState.Success()
+	}
+
+	return info
+}
+
+// Start starts running the service
 func (s *Service) Start() error {
 	return s.cmd.Start()
 }
 
+// Stop stops running the service
 func (s *Service) Stop() error {
 	// TODO: Try to interrupt it first
 	/*
@@ -77,6 +118,7 @@ func (s *Service) Stop() error {
 	return s.cmd.Process.Kill()
 }
 
+// Running returns true if service is currently running
 func (s *Service) Running() bool {
 	if s.cmd.Process != nil && s.cmd.ProcessState == nil {
 		return true
