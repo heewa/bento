5bf5c695ac6c9bd00e96774c847d8982aa0d1d63
Heewa Barfchin <heewa.b@gmail.com>
2016-02-03 18:10:27 -0500

    Handle service run state correctly

--- service/service.go
+++ service/service.go
@@ -2,100 +2,70 @@ package service
 
 import (
 	"fmt"
+	"os"
 	"os/exec"
 	"os/user"
-	"strings"
+	"sync"
+
+	log "github.com/inconshreveable/log15"
 )
 
-// Info holds info about a service
-type Info struct {
+// Service -
+type Service struct {
 	Name    string
 	Program string
 	Args    []string
 	Dir     string
 	Env     map[string]string
 
+	// All these fields are locked by stateLock
+	stateLock sync.RWMutex
+	running   bool
+	pid       int
+	state     *os.ProcessState
+}
+
+// Info holds info about a service
+type Info struct {
+	Service
+
 	Running   bool
 	Pid       int
 	Succeeded bool
 }
 
-// Service -
-type Service struct {
-	Name string
-	cmd  *exec.Cmd
-}
-
 // New creates a new Service
 func New(name string, program string, args []string, dir string, env map[string]string) (*Service, error) {
-	if name == "" {
-		name = program
-	}
-
-	programPath, err := exec.LookPath(program)
-	if err != nil {
-		return nil, err
-	}
-
-	cmd := exec.Command(programPath, args...)
-
-	if dir != "" {
-		cmd.Dir = dir
-	} else {
+	if dir == "" {
 		usr, err := user.Current()
 		if err != nil {
 			return nil, err
 		}
-		cmd.Dir = usr.HomeDir
-	}
-
-	if env != nil {
-		var envItems []string
-		for key, value := range env {
-			envItems = append(envItems, fmt.Sprintf("%s=%s", key, value))
-		}
-		cmd.Env = envItems
-	} else {
-		// Explicitly set it to empty
-		cmd.Env = []string{}
+		dir = usr.HomeDir
 	}
 
 	return &Service{
-		Name: name,
-		cmd:  cmd,
+		Name:    name,
+		Program: program,
+		Args:    args,
+		Dir:     dir,
+		Env:     env,
 	}, nil
 }
 
 // Info gets info about the service
 func (s *Service) Info() Info {
+	s.stateLock.RLock()
+	defer s.stateLock.RUnlock()
+
 	info := Info{
-		Name: s.Name,
-		Dir:  s.cmd.Dir,
+		Service: *s,
+		Running: s.running,
+		Pid:     s.pid,
 	}
 
-	if len(s.cmd.Args) >= 1 {
-		info.Program = s.cmd.Args[0]
-	}
-	if len(s.cmd.Args) >= 2 {
-		info.Args = s.cmd.Args[1:]
-	}
-
-	info.Env = make(map[string]string)
-	for _, item := range s.cmd.Env {
-		parts := strings.Split(item, "=")
-		if len(parts) == 2 {
-			info.Env[parts[0]] = parts[1]
-		} else {
-			info.Env[parts[0]] = ""
-		}
-	}
-
-	if s.Running() {
-		info.Running = true
-		info.Pid = s.cmd.Process.Pid
-	} else {
-		info.Pid = s.cmd.ProcessState.Pid()
-		info.Succeeded = s.cmd.ProcessState.Success()
+	if !s.running && s.state != nil {
+		info.Succeeded = s.state.Success()
 	}
 
 	return info
@@ -103,7 +73,46 @@ func (s *Service) Info() Info {
 
 // Start starts running the service
 func (s *Service) Start() error {
-	return s.cmd.Start()
+	s.stateLock.Lock()
+	defer s.stateLock.Unlock()
+
+	// Clear out previous values
+	s.running = false
+	s.pid = 0
+	s.state = nil
+
+	programPath, err := exec.LookPath(s.Program)
+	if err != nil {
+		return err
+	}
+
+	var envItems []string
+	for key, value := range s.Env {
+		envItems = append(envItems, fmt.Sprintf("%s=%s", key, value))
+	}
+
+	cmd := exec.Command(programPath, s.Args...)
+	cmd.Dir = s.Dir
+	cmd.Env = envItems
+
+	if err := cmd.Start(); err != nil {
+		return err
+	}
+	s.running = true
+	s.pid = cmd.Process.Pid
+
+	go func() {
+		err := cmd.Wait()
+		log.Debug("Service exited", "name", s.Name, "program", s.Program, "err", err)
+
+		s.stateLock.Lock()
+		defer s.stateLock.Unlock()
+
+		s.running = false
+		s.state = cmd.ProcessState
+	}()
+
+	return nil
 }
 
 // Stop stops running the service
@@ -115,13 +124,14 @@ func (s *Service) Stop() error {
 		}
 	*/
 
-	return s.cmd.Process.Kill()
+	//return s.cmd.Process.Kill()
+	return nil
 }
 
 // Running returns true if service is currently running
 func (s *Service) Running() bool {
-	if s.cmd.Process != nil && s.cmd.ProcessState == nil {
-		return true
-	}
-	return false
+	s.stateLock.RLock()
+	defer s.stateLock.RUnlock()
+
+	return s.running
 }
