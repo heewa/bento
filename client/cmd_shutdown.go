1f43ce65f1f62323a0a58b809e06b890dcfd7bb6
Heewa Barfchin <heewa.b@gmail.com>
2016-02-09 15:36:36 -0500

    Fix bug in shutdown client cmd

--- client/cmd_shutdown.go
+++ client/cmd_shutdown.go
@@ -2,6 +2,5 @@ package client
 
 // Shutdown calls the Exit cmd on the Server
 func (c *Client) Shutdown() error {
-	var nothing bool
-	return c.Call("Server.Exit", nothing, nothing)
+	return c.Call("Server.Exit", false, nil)
 }
